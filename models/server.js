232191360d4dfaded7230a3026530398a2f8a0ba
Antonio de Jesus Sanchez Suarez <49320018+AntonioDJSS@users.noreply.github.com>
2023-09-26 16:26:06 -0600

    Update server.js

--- models/server.js
+++ models/server.js
@@ -31,7 +31,7 @@ class Server {
 
     // Middleware de CORS y otros middlewares
     middlewares() {
-        const whitelist = ["http://localhost:5173"];
+        const whitelist = ["http://localhost:5173","https://dbminds.netlify.app/"];
 
         const corsOptions = {
             origin: function (origin, callback) {
