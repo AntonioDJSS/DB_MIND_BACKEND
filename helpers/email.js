163283fa47dfcfb62a640d8404fa1385c7fd1a95
Antonio de Jesus Sanchez Suarez <49320018+AntonioDJSS@users.noreply.github.com>
2023-09-28 01:37:54 -0600

    Update email.js

--- helpers/email.js
+++ helpers/email.js
@@ -1,62 +1,93 @@
 const nodemailer = require('nodemailer');
+const ResponseError = require('../utils/ResponseError');
 
 const emailRegistro = async (datos) =>{
     const { correo, nombre, token } = datos;
 
-    const transport = nodemailer.createTransport({
-        host: "sandbox.smtp.mailtrap.io",
-        port: 2525,
+    try {
+
+      const transport = nodemailer.createTransport({
+        // host: "sandbox.smtp.mailtrap.io",
+        // port: 2525,
+        service: 'SendGrid',
         auth: {
-          user: "b638011704600a",
-          pass: "e7bc714da082be"
+          user: process.env.USERNAMESENDGRID,
+          pass: process.env.APYKEYSENDGRID,
         }
       });
 
+      const confirmarUrl = `${process.env.BASE_URL}/confirmar/${token}`;
+
       //Información del email
       const info = await transport.sendMail({
-        from: '"IKTAN-Tramites" <iktanstrategies@iktanst.com>',
+        from: '"DBMIND" <antoniodjss2000@gmail.com>',
         to: correo,
-        subject: "IKTAN - Comprueba tu cuenta",
-        text: "Comprueba tu cuenta en IKTAN",
-        html: `<p>Hola: ${nombre} Comprueba tu cuenta en IKTAN</p>
+        subject: "DBMIND - Comprueba tu cuenta",
+        text: "Comprueba tu cuenta en DBMIND",
+        html: `<p>Hola: ${nombre} Comprueba tu cuenta en DBMIND</p>
         <p>Tu cuenta ya esta casi lista, solo debes comprobarla en el siguiente enlace:
 
-        <a href="http://localhost:5173/login/confirmar/${token}">Comprobar cuenta</a>
+        <a href="${confirmarUrl}">Comprobar cuenta</a>
 
         <p>Si tu no creaste esta cuenta, puedes ignorar el mensaje</p>
         
         `,
       })
+    } catch (ex) {
+      const response = new ResponseError(
+        'fail',
+        'Error al enviar el correo del registro mediante Desarrollo',
+        ex.message,
+      []).responseApiError();
+
+       // Devolver la respuesta de error utilizando tu clase ResponseError
+       console.log(ex)
+       return response.responseApiError();
+    }
 }
 
 
 const emailOlvidePassword = async (datos) =>{
   const { correo, nombre, token } = datos;
 
+  try {
   const transport = nodemailer.createTransport({
-      host: "sandbox.smtp.mailtrap.io",
-      port: 2525,
-      auth: {
-        user: "b638011704600a",
-        pass: "e7bc714da082be"
-      }
+      // host: "sandbox.smtp.mailtrap.io",
+      // port: 2525,
+      service: 'SendGrid',
+        auth: {
+          user: process.env.USERNAMESENDGRID,
+          pass: process.env.APYKEYSENDGRID,
+        }
     });
+
+    const olvideUrl = `${process.env.BASE_URL}/olvide-password/${token}`;
 
     //Información del email
     const info = await transport.sendMail({
-      from: '"IKTAN-Tramites" <iktanstrategies@iktanst.com>',
+      from: '"DB-Mind" <antoniodjss2000@gmail.com>',
       to: correo,
-      subject: "IKTAN - Restablece tu password",
+      subject: "DB-Mind - Restablece tu password",
       text: "Restablece tu password",
       html: `<p>Hola: ${nombre} has solicitado reestablecer tu password</p>
       <p>Sigue el siguiente enlace para generar un password:
 
-      <a href="http://localhost:5173/login/olvide-password/${token}">Reestablecer Password</a>
+      <a href="${olvideUrl}">Reestablecer Password</a>
 
       <p>Si tu no solicitaste este email, puedes ignorar el mensaje</p>
       
       `,
     })
+  } catch (ex) {
+    const response = new ResponseError(
+      'fail',
+      'Error al enviar el correo de olvidePassword mediante Desarrollo',
+      ex.message,
+    []).responseApiError();
+
+     // Devolver la respuesta de error utilizando tu clase ResponseError
+     return response.responseApiError();
+  }
 }
 
 
